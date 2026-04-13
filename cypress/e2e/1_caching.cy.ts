effd39710d744eaf5d1374051162246cac69bac4
yrouxel <yoann.rouxelduval@gmail.com>
2026-04-13 22:58:59 +0200

    clean(type) removed js type hints

--- cypress/e2e/1_caching.cy.ts
+++ cypress/e2e/1_caching.cy.ts
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 context('Cache Verification', () => {
   it('First Run: Cache Miss - Loads slowly natively (takes > 500ms)', () => {
     cy.visit('/', { timeout: 1000 })
--- cypress/e2e/2_cached.cy.ts
+++ cypress/e2e/2_cached.cy.ts
@@ -1,5 +1,3 @@
-/// <reference types="cypress" />
-
 context('Cache Verification', () => {
   it('Second Run: Cache Hit - Loads fast (takes < 100ms)', () => {
     cy.visit('/', { timeout: 100 })
